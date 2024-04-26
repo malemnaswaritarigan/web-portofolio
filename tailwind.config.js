bc33e9fe737610eb15978ee2c8e423ffb2fc3439
malemnaswaritarigan <malemnaswaritarigan@gmail.com>
2024-04-26 22:23:40 +0700

    debut portofolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  darkMode: "class",
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1e1b4b",
+      },
+      spacing: {
+        big: "48rem",
+      },
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    fontFamily: {
+      nunito: ["Nunito", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
